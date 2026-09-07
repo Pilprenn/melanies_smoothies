9b20dcb93d4fedbb08a59316ee108f6655547339
Loïc Le Quéré <115620808+Pilprenn@users.noreply.github.com>
2026-09-07 17:45:08 +0200

    Update streamlit_app.py
    dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,5 @@ if ingredients_list:
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
-#response = requests.get(
-#"https://my.smoothiefroot.com/api/fruit/watermelon"
-#)
-#st.write(response.status_code)
-#st.write(response.text)
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
