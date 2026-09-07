760cde4380000d0c0f7a1f07710ab6b6844c547e
Loïc Le Quéré <115620808+Pilprenn@users.noreply.github.com>
2026-09-07 18:19:07 +0200

    Update streamlit_app.py
    add search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,9 @@ st.write("""Choose the fruit you want in your custom smoothie !""")
 name_on_order = st.text_input('Name on smoothie: ')
 st.write('The name on your smoothie will be: ', name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
